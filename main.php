158b382791ceab2c52bd6a18eadda0bc1032a160
Maxime Piscaglia <maximepiscaglia@hotmail.com>
2021-11-16 11:15:50 +0100

    projet set up

--- main.php
+++ main.php
@@ -1,2 +1,18 @@
 <?php
 
+declare(strict_types=1);
+
+// Affichage des erreurs
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
+// Autoloader
+require __DIR__ . '/vendor/autoload.php';
+
+// Query use
+use hellokant\query\Query;
+
+//////////////////////////////:://///////
+
+Query::test();
---
+++ src/query/Query.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace hellokant\query;
+
+class Query {
+
+    static public function test() {
+        echo "autoloader ok";
+    }
+
+}
