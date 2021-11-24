363fdfea0585b98f27eafbb9b4414f9463421dae
Maxime Piscaglia <maximepiscaglia@hotmail.com>
2021-11-24 12:05:01 +0100

    makeConnection of ConnectionFactory made

--- src/query/ConnectionFactory.php
+++ src/query/ConnectionFactory.php
@@ -4,12 +4,25 @@ namespace hellokant\query;
 
 class ConnectionFactory {
 
+    private static $db;
+
     public static function makeConnection(array $conf) {
-        var_dump($conf);
-        echo "YES";
+        $params = array(
+            PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_EMULATE_PREPARES=>false, // comprendre !
+            PDO::ATTR_STRINGIFY_FETCHES=>false
+        );
+        
+        // Data source name
+        $dsn="mysql:host=$conf[serveur];dbname=$conf[database]";
+        $db = new \PDO($dsn, $conf[user], $conf[pass], $params);
+        return self::$db;
     }
 
     public static function getConnection() {
         echo "getConnection to do";
     }
 }
+
+// FINIR TD2, MODEL
+// PUIS MONGO DB
